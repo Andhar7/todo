85a6dc4bd01e3b2e080425442be1b2a3ee05a40a
Andhar7 <boxingprofi@web.de>
2025-08-19 17:53:13 +0200

    mark_as_done

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,14 @@
 # from django.http import HttpResponse
-from django.shortcuts import redirect 
+from django.shortcuts import get_object_or_404, redirect 
 from .models import Todo
 
 def addTask(request):
     task = request.POST['task']
     Todo.objects.create(title=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Todo, pk=pk)
+    task.completed = True
+    task.save()
     return redirect('home')
